8e4e2a48fcf973bbcc242f2aa22f08aacd0f47c7
eidam-slices <adam.rabasek@gmail.com>
2025-11-20 22:33:27 +0100

    feat(category): add DrawPreferenceCategory (similar to DrawPreferenceRow) for rendering category and its content

--- material_preferences/src/main/java/cz/eidam/material_preferences/generic/ui/DrawPreferenceRow.kt
+++ material_preferences/src/main/java/cz/eidam/material_preferences/generic/ui/DrawPreferenceRow.kt
@@ -4,6 +4,12 @@ import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import cz.eidam.material_preferences.composable.choice.PersistentPreferenceChoice
+import cz.eidam.material_preferences.category.ui.DrawPreferenceCategory
+import cz.eidam.material_preferences.composable.multi_choice.PersistentPreferenceMultiChoice
+import cz.eidam.material_preferences.composable.range_slider.PersistentPreferenceRangeSlider
+import cz.eidam.material_preferences.composable.slider.PersistentPreferenceSlider
+import cz.eidam.material_preferences.composable.switch.PersistentPreferenceSwitch
 import cz.eidam.material_preferences.generic.model.PreferenceRow
 
 
