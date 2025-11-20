bec894f2077615726c94aa27624535f034393a39
eidam-slices <adam.rabasek@gmail.com>
2025-11-20 22:32:13 +0100

    feat: add dataStore parameter to DrawPreferenceRow for saving

--- material_preferences/src/main/java/cz/eidam/material_preferences/generic/ui/DrawPreferenceRow.kt
+++ material_preferences/src/main/java/cz/eidam/material_preferences/generic/ui/DrawPreferenceRow.kt
@@ -2,12 +2,15 @@ package cz.eidam.material_preferences.generic.ui
 
 import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import cz.eidam.material_preferences.generic.model.PreferenceRow
 
 
 @Composable
 internal fun DrawPreferenceRow(
     preference: PreferenceRow,
+    dataStore: DataStore<Preferences>
 ) {
     when (val preference = preference) {
         else -> Log.e("DrawPreferenceRow", "Failed to draw PreferenceRow. Unsupported PreferenceRow type: $preference")
