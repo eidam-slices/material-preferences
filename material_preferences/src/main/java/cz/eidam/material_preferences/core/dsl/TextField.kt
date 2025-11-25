153941d37bded5bb8448cf9543aac70ec5408fd5
eidam-slices <adam.rabasek@gmail.com>
2025-11-25 23:06:42 +0100

    feat(text): add dsl functions for text field preferences

---
+++ material_preferences/src/main/java/cz/eidam/material_preferences/core/dsl/TextField.kt
@@ -0,0 +1,65 @@
+package cz.eidam.material_preferences.core.dsl
+
+import androidx.annotation.StringRes
+import cz.eidam.material_preferences.core.model.Text
+import cz.eidam.material_preferences.text.model.TextPreference
+
+// String Overload
+fun PreferenceScope.textField(
+    key: String,
+    title: String,
+    description: String? = null,
+    defaultValue: String,
+) = add {
+    TextPreference.Modal(
+        key = key,
+        title = Text(title)!!,
+        description = Text(description),
+        defaultValue = defaultValue
+    )
+}
+
+// String Resource Overload
+fun PreferenceScope.textField(
+    key: String,
+    @StringRes title: Int,
+    @StringRes description: Int? = null,
+    defaultValue: String,
+) = add {
+    TextPreference.Modal(
+        key = key,
+        title = Text(title)!!,
+        description = Text(description),
+        defaultValue = defaultValue,
+    )
+}
+
+// String / Inline Overload
+fun PreferenceScope.inlineTextField(
+    key: String,
+    title: String,
+    description: String? = null,
+    defaultValue: String,
+) = add {
+    TextPreference.Inline(
+        key = key,
+        title = Text(title)!!,
+        description = Text(description),
+        defaultValue = defaultValue
+    )
+}
+
+// String Resource / Inline Overload
+fun PreferenceScope.inlineTextField(
+    key: String,
+    @StringRes title: Int,
+    @StringRes description: Int? = null,
+    defaultValue: String,
+) = add {
+    TextPreference.Inline(
+        key = key,
+        title = Text(title)!!,
+        description = Text(description),
+        defaultValue = defaultValue,
+    )
+}
