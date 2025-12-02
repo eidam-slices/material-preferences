a9b683a99c44761369a3eb2b7b4dbdb0c6348fc5
eidam-slices <adam.rabasek@gmail.com>
2025-12-02 01:55:20 +0100

    feat(multi choice): add persistent stateful multi choice trough data store

---
+++ material_preferences/src/main/java/cz/eidam/material_preferences/multichoice/ui/DataStorePreferenceMultiChoice.kt
@@ -0,0 +1,44 @@
+package cz.eidam.material_preferences.multichoice.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import cz.eidam.material_preferences.core.utils.dataStore
+import cz.eidam.material_preferences.core.utils.getStringSetState
+import cz.eidam.material_preferences.core.utils.setStringSet
+import kotlinx.coroutines.launch
+
+@Composable
+fun DataStorePreferenceMultiChoice(
+    key: String,
+    title: String,
+    description: String? = null,
+    entries: List<String>,
+    entryValues: List<String>,
+    defaultValue: Set<String>,
+    tooltipEnabled: Boolean = true,
+    valueDisplayFormatter: (List<String>) -> String = MultiChoicePreferenceDefaults.formatter(),
+    dataStore: DataStore<Preferences> = LocalContext.current.dataStore,
+) {
+    val values by dataStore.getStringSetState(key, defaultValue)
+
+    val scope = rememberCoroutineScope()
+    PreferenceMultiChoice(
+        title = title,
+        description = description,
+        entries = entries,
+        entryValues = entryValues,
+        values = values,
+        onValueChange = { updated ->
+            scope.launch {
+                dataStore.setStringSet(key, updated)
+            }
+        },
+        tooltipEnabled = tooltipEnabled,
+        valueDisplayFormatter = valueDisplayFormatter,
+    )
+
+}
