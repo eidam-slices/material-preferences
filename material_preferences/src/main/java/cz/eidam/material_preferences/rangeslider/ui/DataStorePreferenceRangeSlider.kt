5453bde0d92e4fd7f5a2e77a48ee39412c5f01e6
eidam-slices <adam.rabasek@gmail.com>
2025-12-02 01:55:40 +0100

    feat(range slider): add persistent stateful ranger slider trough data store

---
+++ material_preferences/src/main/java/cz/eidam/material_preferences/rangeslider/ui/DataStorePreferenceRangeSlider.kt
@@ -0,0 +1,43 @@
+package cz.eidam.material_preferences.rangeslider.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import cz.eidam.material_preferences.core.utils.dataStore
+import cz.eidam.material_preferences.core.utils.getFloatRangeState
+import cz.eidam.material_preferences.core.utils.setFloatRange
+import kotlinx.coroutines.launch
+
+@Composable
+fun DataStorePreferenceRangeSlider(
+    key: String,
+    title: String,
+    description: String? = null,
+    defaultValue: ClosedFloatingPointRange<Float>,
+    valueRange: ClosedFloatingPointRange<Float>,
+    step: Float,
+    transform: (ClosedFloatingPointRange<Float>) -> String =
+        RangeSliderPreferenceDefaults.unitFormatter(step, ""),
+    dataStore: DataStore<Preferences> = LocalContext.current.dataStore,
+) {
+    val value by dataStore.getFloatRangeState(key, defaultValue)
+
+    val scope = rememberCoroutineScope()
+    PreferenceRangeSlider(
+        title = title,
+        description = description,
+        valueRange = valueRange,
+        step = step,
+        transform = transform,
+
+        value = value,
+        onValueChange = { updated ->
+            scope.launch {
+                dataStore.setFloatRange(key, updated)
+            }
+        }
+    )
+}
